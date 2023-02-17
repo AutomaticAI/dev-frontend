4a7f11fe6cabc2e0525d556600da1f14d3a67490
leo <townhelpemail@gmail.com>
2023-02-17 16:21:48 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ let lastMessageId;
 export const fetchResponse = async chat => {
 	try {
 		// after depoloyment you should change the fetch URL below
-		const response = await fetch('https://apibot.autoai.site', {
+		const response = await fetch('https://dev-backend-xozz.vercel.app/', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
